24d5acb5161f629bf2ffbf8397ab27598347893e
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2017-04-15 17:43:40 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+echo `ls`
+rm -rf docs || exit 0;
+mkdir docs
+
+cp -r public/* docs
+cd docs
+
+git config user.email "example@example.com"
+git config user.name "Travis-CI"
+
+git add .
+git commit -am "Deploy blog"
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:master > /dev/null 2>&1
