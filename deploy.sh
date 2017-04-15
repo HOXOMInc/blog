0b370166ffcf5c4d3f92ee03a5a1635dd568351a
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2017-04-15 15:40:07 +0900

    Update dynamically

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+
+git config --global user.email "jtrecenti@jtrecenti.com"
+git config --global user.name "jtrecenti"
+
+git clone -b https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git blog
+cd blog
+rm public/.gitignore
+cp -rf public/* docs
+git add docs/*
+git commit -m "Update" || true
+git push origin master
