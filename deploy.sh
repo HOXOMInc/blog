bb7b840981f9eb6c288eb36e33843b5b2e55adb4
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2017-04-15 17:06:44 +0900

    Update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 git config --global user.email "tomomoto@mail.hoxo-m.com"
 git config --global user.name "tomomoto"
 
-git clone -b https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git blog
+echo ${GITHUB_PAT}
+echo ${TRAVIS_REPO_SLUG}.git
+git clone https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git
 cd blog
 rm public/.gitignore
 cp -rf public/* docs
